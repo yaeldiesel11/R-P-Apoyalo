143ce2bce336e9431963b283317fc1f1a59a6911
yaeldiesel11 <astr2109@gmail.com>
2024-05-13 05:01:03 -0400

    commit

--- src/componentes/App/App.jsx
+++ src/componentes/App/App.jsx
@@ -1,11 +1,53 @@
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hola clase</h1>
-    </div>
-  );
+class PersonCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listPeople: [{
+        firstName: "Jane",
+        lastName: "Doe",
+        age: "45",
+        hairColor: "Black"
+      },
+      {
+        firstName: "John",
+        lastName: "Smith",
+        age: "88",
+        hairColor: "Brown"
+      },
+      {
+        firstName: "Millard",
+        lastName: "Fillmore",
+        age: "50",
+        hairColor: "Brown"
+      },
+      {
+        firstName: "Maria",
+        lastName: "Smith",
+        age: "62",
+        hairColor: "Brown"
+      }]
+    }
+  }
+  render = () => {
+    return (
+      <div>
+        <ul>
+          {this.state.listPeople.map((people, index) => {
+            return (
+              <li key={index}>
+                <h2>{people.lastName}, {people.firstName}</h2>
+                <p>Age: {people.age}</p>
+                <p>Hair color: {people.hairColor}</p>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default PersonCard;
